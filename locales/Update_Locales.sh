46af734103f17c0e97d71270c58897258e5121af
BlueArduino20 <bluearduino20@gmail.com>
2017-05-19 20:01:57 +0200

    Update Update_Locales.sh

--- locales/Update_Locales.sh
+++ locales/Update_Locales.sh
@@ -1,40 +1,29 @@
 rm ./*.lang 
-clear 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/en_US.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/en_UK.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/de_DE.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/fr_FR.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/be_BE.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/es_ES.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/fi_FI.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/cz_CZ.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/da_DK.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/pt_PT.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/tr_TR.lang 
-clear 
 echo Updating... Please, wait. 
 wget https://raw.githubusercontent.com/Nurrl/LocaleKeyboard.js/master/locales/it_IT.lang 
 clear 
-echo "-----------------------------"
-echo "            Done!"
-echo "-----------------------------"
+echo "------------------------------------------"
+echo "                   Done!"
+echo "------------------------------------------"
+../LocaleKeyboard.SH 
